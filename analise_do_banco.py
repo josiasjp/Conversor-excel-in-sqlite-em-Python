2e46b28953819ab9817f8fe55d60afb0c555a4d3
josias <josias@pop-os.localdomain>
2021-02-11 11:42:49 -0300

    Analise de Dados

---
+++ analise_do_banco.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sqlite3 as sql
+from IPython.display import display
+import plotly.express as px
+
+conn = sql.connect('banco.db')
+
+tabela = pd.read_sql('SELECT * FROM Clientes', conn)
+display(tabela.info())
+conn.close()
+
+def gerar_grafico(coluna, tabela):
+    grafico_histograma = px.histogram(tabela, x=coluna, color='Categoria')
+    grafico_histograma.show()
+
+for coluna in tabela:
+    gerar_grafico(coluna, tabela)
