59b2da064dd79cb7e6c046bc58f337efa873e080
josias <josias@pop-os.localdomain>
2021-02-11 13:58:19 -0300

    transformar tabela excel em tabela de banco sqlite

---
+++ Excel_Sqlite.py
@@ -0,0 +1,12 @@
+import sqlite3 as sql
+from numpy import append
+import pandas as pd
+from IPython.display import display
+from pandas.core.indexes.base import Index
+
+tabela_cliete = pd.read_csv('ClientesBanco.csv', encoding='latin1')
+
+conexao = sql.connect('banco.db')
+tabela_cliete.to_sql('Clientes', con=conexao, if_exists='append', index=False) #transfere um excel para uma tabela
+conexao.close()
+
