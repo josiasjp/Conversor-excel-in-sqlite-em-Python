9df7fc5f1ca1f5cef071f35125abf9df15595a21
josiasjp <josiasdesenvolvedor@gmail.com>
2022-11-28 00:32:33 -0300

    correção no comentário do codigo

--- Excel_Sqlite.py
+++ Excel_Sqlite.py
@@ -7,6 +7,6 @@ from pandas.core.indexes.base import Index
 tabela_cliete = pd.read_csv('ClientesBanco.csv', encoding='latin1')
 
 conexao = sql.connect('banco.db')
-tabela_cliete.to_sql('Clientes', con=conexao, if_exists='append', index=False) #transfere um excel para uma tabela
+tabela_cliete.to_sql('Clientes', con=conexao, if_exists='append', index=False) #transfere um excel para um arquivo sqlite
 conexao.close()
 
